483c5d0a5ecc4ba56880a31c74631bef15fca193
cinexin <cats_dont_fly@yahoo.es>
2017-05-26 10:30:58 +0200

    Lyrics class. Implementation of showLyrics()

---
+++ KaraokeCli/Lyrics.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Security;
+
+namespace KaraokeCli
+{
+	public class Lyrics
+	{
+		private string[] lines;
+
+		private void WriteLine(string line)
+		{
+			var results = line.Split(':');
+			this.WriteLine(results[0], int.Parse(results[1]));
+
+		}
+
+		private void WriteLine(string line, int time)
+		{
+			var speed = time / line.Length;
+			foreach (var letter in line.ToCharArray())
+			{
+				Console.Write(letter);
+				System.Threading.Thread.Sleep(speed);
+			}
+			Console.WriteLine("");
+
+		}
+
+		public void ShowLyrics()
+		{
+			Console.WriteLine("============================================");
+			foreach (var line in lines)
+			{
+				this.WriteLine(line);
+			}
+			Console.WriteLine("============================================");
+		}
+
+		public Boolean InitLyricsFromFile(string fileName)
+		{
+			try
+			{
+				this.lines = File.ReadAllLines(fileName);
+			}
+			catch (Exception ex ) when (
+				ex is ArgumentException ||
+				ex is ArgumentNullException ||
+				ex is PathTooLongException ||
+				ex is DirectoryNotFoundException ||
+				ex is IOException ||
+				ex is UnauthorizedAccessException ||
+				ex is FileNotFoundException ||
+				ex is SecurityException
+			)
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public Lyrics()
+		{
+		}
+	}
+}
--- KaraokeCli/Program.cs
+++ KaraokeCli/Program.cs
@@ -1,13 +1,49 @@
 ﻿using System;
 
+
 namespace KaraokeCli
 {
 	class MainClass
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Welcome to Karaoke Command-Line!");
-			Console.ReadLine();
+			const string fileSuffix = ".txt";
+			var songs = new string[] { "allthistime","endoftheworld","itsasmallworld"};
+			while (true)
+			{
+				Console.WriteLine("Welcome to Karaoke Command-Line!");
+				Console.WriteLine("Choose the song you'd like to sing...");
+				Console.WriteLine("1 for All This Time");
+				Console.WriteLine("2 for End Of The World");
+				Console.WriteLine("3 for It's a Small World");
+				Console.Write("Your choice (type 'Q' to quit app): ");
+
+				var response = Console.ReadLine();
+
+				if ( response.ToLower().StartsWith("q")  )
+				{
+					break;
+					//Console.ReadLine();
+				}
+
+				var chosenSong = 0;
+				if (!int.TryParse(response, out chosenSong))
+				{
+					Console.WriteLine("Ooops! Wrong choice! Try again...");
+					continue;	
+				}
+
+				chosenSong--;
+				var fileName = songs[chosenSong] + fileSuffix;
+				Lyrics lyrics = new Lyrics();
+				if (!lyrics.InitLyricsFromFile(fileName))
+				{
+					Console.WriteLine("Ooops...there was an error trying to open the lyrics. Try another option. Sorry :-(");
+					continue;
+				}
+
+				lyrics.ShowLyrics();
+			}
 		}
 	}
 }
